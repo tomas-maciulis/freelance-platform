34606dc771685a9b29278ba24e86a12fe118410e
Tomas Mačiulis <tomas.maciulis4@gmail.com>
2020-05-11 20:39:35 +0300

    Fix duplicate text in `/cv` page if cv list is empty

--- resources/views/asset/cv/list.blade.php
+++ resources/views/asset/cv/list.blade.php
@@ -1,8 +1,3 @@
 @foreach($cvs as $cv)
     @include('asset.cv.card', [$cv, $user])
 @endforeach
-@if(!$cvs->count())
-    <div class="flex justify-center">
-        You have no CVs. Create one now!
-    </div>
-@endif
